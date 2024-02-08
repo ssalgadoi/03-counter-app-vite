fc3b39dc9e99f9d551599c0b62b856cc865c0191
Sebastián <se.salgado@duocuc.cl>
2024-02-08 17:23:58 -0300

    Hola mUndogit add .

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import  ReactDOM from 'react-dom/client'
+
+
+function App() {
+    return <h1>Hola mUndo, soy Shipy Developer!!!!</h1>;
+}
+
+ReactDOM.createRoot( document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
